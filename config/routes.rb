b46a7ccdd7334f9818b7041d5f736cddfa4269b3
miguel Borquez <miguelborquez7@gmail.com>
2024-08-13 12:20:31 -0700

    devise user implementation migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'static_pages/home'
   get 'static_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
