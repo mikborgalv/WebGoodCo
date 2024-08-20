72582ee51e7e0871e30cf5ba140f7e068758fd56
miguel Borquez <miguelborquez7@gmail.com>
2024-08-20 12:27:06 -0700

    static pages controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,12 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get static pages home" do
+  test "should get home" do
     get static_pages_home_url
     assert_response :success
   end
 
-  test "should get static pages about" do
+  test "should get about" do
     get static_pages_about_url
     assert_response :success
   end
