ac6b2356f9790754fd3a852b78f6eaef85d6820b
miguel Borquez <miguelborquez7@gmail.com>
2024-08-24 23:01:26 -0700

    iplemented sign up and resiter

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static_pages/home'
   get 'static_pages/about'
+  get 'static_pages/home'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
